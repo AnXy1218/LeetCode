f61a38cce92c204516c72fe43a98925b7c3c8edd
axy <jssaxy@163.com>
2020-03-03 22:21:07 +0800

    交换节点

---
+++ leetcode/src/com/leetcode/question/LeetCode24.java
@@ -0,0 +1,25 @@
+package com.leetcode.question;
+
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class LeetCode24 {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode temp = head.next;
+        head.next = head.next.next;
+        temp.next = head;
+        temp.next.next = swapPairs(temp.next.next);
+        return temp;
+    }
+}
